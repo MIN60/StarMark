6289b73bc15fee33221138abd5001b2a4280e0dc
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-19 17:40:00 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import { Dashboard } from './dashboard/dash.js';
+import { Login } from './logmain/login.js';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <Routes>
+              <Route path='/Dashboard' element={<Dashboard />}></Route>
+              <Route path='/Login' element={<Login />}></Route>
+
+          </Routes>
+        </Router>
+      </div>
+    </div>    
   );
 }
 
