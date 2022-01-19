28ccb2af204b6b16bf85a06fb6b17bd9b3713644
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-20 00:16:55 +0900

    Add Login

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import { dash } from './dashboard/dash.js';
-
+import { Login } from './home/Login.js';
 
 
 function App() {
   return (
     <div className="App">
+      <div id = "login"></div>
       <div>
         <Router>
           <Routes>
               <Route path='/dashboard' element={<dash />}></Route>
+              <Route path='/Login' element={<Login />}></Route>
           </Routes>
         </Router>
       </div>
