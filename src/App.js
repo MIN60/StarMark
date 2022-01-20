504f8135673f16801109f8d0aec3034fc159e7d3
metamong-Hi <seojinlee27@naver.com>
2022-01-20 12:38:40 +0900

    error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css';
-import { Dashboard } from './dashboard/dash.js';
-import { Login } from './logmain/login.js';
-
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Login } from "./logmain/login.js";
 
 function App() {
   return (
@@ -14,13 +10,11 @@ function App() {
       <div>
         <Router>
           <Routes>
-              <Route path='/Dashboard' element={<Dashboard />}></Route>
-              <Route path='/Login' element={<Login />}></Route>
-
+            <Route path="/Login" element={<Login />}></Route>
           </Routes>
         </Router>
       </div>
-    </div>    
+    </div>
   );
 }
 
